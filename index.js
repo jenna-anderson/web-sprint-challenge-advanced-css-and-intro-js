d1d2effb247239bb17d95b24abb6f79a4ce40e5c
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-04-16 10:11:55 -0600

    Create a CodeGrade submission

--- index.js
+++ index.js
@@ -345,7 +345,7 @@ The function should console.log 50 chunks of HTML code that match the structure 
 
 ‼️ You do **NOT** need to get these to display on your page, but you can copy and paste the result into your HTML file if you'd like to see what that would look like. */
 
-function getHTML(/* Code here */){
+function getHTML(data){
 
     /* Code here */
 
